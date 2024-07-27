28bcf580d71ac47a3168b6efa0ef2f9189924106
UjjwalOnGit <uj643570ujjwal@gmail.com>
2024-07-27 16:44:45 +0530

    Added a school program

---
+++ NumberLine.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberLine {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[10];
+
+    System.out.println("Enter 10 numbers:");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    System.out.print("Numbers Line: ");
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+        System.out.print(arr[i] + ", ");
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] >= 0) {
+        System.out.print(arr[i] + ", ");
+      }
+    }
+    sc.close();
+  }
+}
