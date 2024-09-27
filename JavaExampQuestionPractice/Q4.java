f3df79a37fca50af6368be55cd6565a64961027a
Ujjwal Singh <uj643570ujjwal@gmail.com>
2024-09-27 06:47:16 +0530

    hi

---
+++ JavaExampQuestionPractice/Q4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 01;
+        if (a == b) {
+            System.out.println("1 = 01");
+        } else if (a != b) {
+            System.out.println("1 is not = 01");
+        }
+    }
+}
