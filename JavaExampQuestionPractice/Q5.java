3c2b2a8bc1c69c397b417d30ef533d1de1278036
Ujjwal Singh <uj643570ujjwal@gmail.com>
2024-09-28 17:32:43 +0530

    done

--- JavaExampQuestionPractice/Q5.java
+++ JavaExampQuestionPractice/Q5.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 1; i < 10 ; i++){
-            System.out.println("enter :"+i+"th element in your array");
-            int arr[i] = sc.nextInt();
-
-            System.out.println(arr);
+        int arr[] = new int[10];
+        for(int i = 0; i < 10 ; i++){
+            System.out.println("enter   "+i+" th   element in your array");
+            arr[i] = sc.nextInt();
         }
     }
 }
